0ab7223830c1d850e952ba5049687b24affc68bf
rn-mgl <reinb2ra@gmail.com>
2023-02-22 15:11:25 +0800

    error handler for repeated email

--- middleware/error-middleware.js
+++ middleware/error-middleware.js
@@ -8,6 +8,16 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || "Sorry, we are currently having internal errors. Try again later.",
   };
 
+  if (err.name === "CastError") {
+    customError.statusCode = StatusCodes.NOT_FOUND;
+    customError.msg = `No order found with the id ${err.value.id || err.value}`;
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.statusCode = 400;
+    customError.msg = `Value entered for ${Object.keys(err.keyValue)} already exists.`;
+  }
+
   res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
